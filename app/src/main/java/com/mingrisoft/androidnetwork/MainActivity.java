773a0a9522e4bb9119ca7b3726315d1035e9d5d0
herguilin <1947538125@qq.com>
2021-03-01 22:33:50 +0800

    Initial commit，在网络配置文件添加了注释

--- app/src/main/java/com/mingrisoft/androidnetwork/MainActivity.java
+++ app/src/main/java/com/mingrisoft/androidnetwork/MainActivity.java
@@ -37,16 +37,16 @@ public class MainActivity extends AppCompatActivity {
         ResultAdapter resultAdapter;
         resultAdapter = new ResultAdapter();
         recyclerView.setAdapter(resultAdapter);*/
-
     }
 
+    //第4课：使用java的API进行数据请求
     public void loadJson(View view) {
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    URL url = new URL("https://www.sunofbeach.net");
-                    //URL url = new URL("http://10.0.2.2:9102/get/text");
+                   // URL url = new URL("https://www.sunofbeach.net");
+                    URL url = new URL("http://10.0.2.2:9102/get/text");
                     HttpURLConnection connection = (HttpURLConnection) url.openConnection();//打开链接
                     connection.setRequestMethod("GET");//八种常用方法get put.....
                     connection.setRequestProperty("Accept-Language", "zh-CN,zh;q=0.9");//请求头设置
@@ -57,28 +57,35 @@ public class MainActivity extends AppCompatActivity {
                         Map<String, List<String>> headerFields = connection.getHeaderFields();
                         Set<Map.Entry<String, List<String>>> entries = headerFields.entrySet();
                         //entries.for
-                /*
-                打印头部信息，请求头
-                 */
-                        for (Map.Entry<String, List<String>> entry : entries) {
-                            Log.d(TAG, entry.getKey() + "==" + entry.getValue());
-                        }
-                        Object content = connection.getContent();
-                        Log.d(TAG, "content------>" + content);
+                        /*
+                        打印头部信息，请求头
+                         */
+                        //for (Map.Entry<String, List<String>> entry : entries) {
+                        //    Log.d(TAG, entry.getKey() + "==" + entry.getValue());
+                        //}
+
+                        //Object content = connection.getContent();
+                        //Log.d(TAG, "content------>" + content); //输出的是一个buffer
+
+                        //我们希望拿到的是一个字符串，所以要用流
+                        InputStream inputStream = connection.getInputStream();
+                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                        String line = bufferedReader.readLine();
+                        Log.d(TAG, "Line------->" + line);
                     }
-                    InputStream inputStream = connection.getInputStream();
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                    String line = bufferedReader.readLine();
-                    Log.d(TAG, "Line------->" + line);
+
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         }).start();
-
     }
+    //第4课：使用java的API进行数据请求,到这截止
 
-    public void topicture(View view) {
+    /****
+    跳转
+     ****/
+    public void toPicture(View view) {
         Intent intent = new Intent(MainActivity.this,PicLoadActivity.class);
         startActivity(intent);
     }
